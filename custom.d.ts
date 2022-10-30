b4a7ca08f1ef34590509008c3a527173db03d21c
dmitry2205 <dmitry2205@gmail.com>
2022-10-30 15:44:42 +0300

    Config

---
+++ custom.d.ts
@@ -0,0 +1,4 @@
+declare module '*(.svg|.png|.jpg|.jpeg|.gif|.ico)' {
+    const content: any;
+    export default content;
+  }
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    hot:true,
+  },
+  devtool: 'inline-source-map'
+};
